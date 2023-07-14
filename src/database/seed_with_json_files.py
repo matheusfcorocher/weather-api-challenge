91d60990b96baca618528f1b8ba38c82a1e36526
matheusfc <matheuscorocher@gmail.com>
2023-07-14 10:57:33 -0300

    refactor: create inversion of dependency of seed_with_json_files fn

--- src/database/seed_with_json_files.py
+++ src/database/seed_with_json_files.py
@@ -3,18 +3,27 @@ import os
 
 from database_connection import mongo_connection
 
-def seed_mongo_database_with_json_files(db, seed_path, collection_name):
-    seed_dir = seed_path
-    collection = db[collection_name]
+def make_seed_mongo_database_with_json_files(directory_lister, path_joiner, json_loader):
 
-    # Delete all documents from the collection
-    collection.delete_many({})
+    def seed_mongo_database_with_json_files(db, seed_path, collection_name):
+        seed_dir = seed_path
+        collection = db[collection_name]
+
+        # Delete all documents from the collection
+        collection.delete_many({})
+        
+        for filename in directory_lister(seed_dir):
+            if filename.endswith('.json'):
+                with open(path_joiner(seed_dir, filename)) as file:
+                    data = json_loader(file)
+                    collection.insert_one(data)
     
-    for filename in os.listdir(seed_dir):
-        if filename.endswith('.json'):
-            with open(os.path.join(seed_dir, filename)) as file:
-                data = json.load(file)
-                collection.insert_one(data)
+    return seed_mongo_database_with_json_files
 
 db = mongo_connection.get_weather_db()
-seed_mongo_database_with_json_files(db, './data/seeds', "weather")
+seed_with_json_files = make_seed_mongo_database_with_json_files(
+    directory_lister=os.listdir, 
+    path_joiner=os.path.join, 
+    json_loader=json.load
+)
+seed_with_json_files(db, './data/seeds', "weather")
