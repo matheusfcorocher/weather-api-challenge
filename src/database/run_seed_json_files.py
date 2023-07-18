f097314e592526de9fe50da985de05cabf4e5533
matheusfc <matheuscorocher@gmail.com>
2023-07-18 13:52:05 -0300

    refactor: change collection name from weather to data

--- src/database/run_seed_json_files.py
+++ src/database/run_seed_json_files.py
@@ -5,7 +5,7 @@ from database_connection import mongo_connection
 from seed_with_json_files import make_seed_mongo_database_with_json_files
 
 db = mongo_connection.get_weather_db()
-collection = db["weather"]
+collection = db["data"]
 
 seed_with_json_files = make_seed_mongo_database_with_json_files(
     directory_lister=os.listdir, 
